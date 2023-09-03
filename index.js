93088f83b30339791ce4e654ec4ac2ea607c3358
Juliano Krindges <juliano9krindges@hotmail.com>
2023-09-02 22:07:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+const lista_produtos = {
+  produtos: [
+    {
+      id: 1,
+      descricao: 'Arroz parboilizado 5Kg',
+      valor: 25.0,
+      marca: 'Tio João',
+    },
+    { id: 2, descricao: 'Maionese 250gr', valor: 7.2, marca: 'Helmans' },
+    { id: 3, descricao: 'Iogurte Natural 200ml', valor: 2.5, marca: 'Itambé' },
+    {
+      id: 4,
+      descricao: 'Batata Maior Palha 300gr',
+      valor: 15.2,
+      marca: 'Chipps',
+    },
+    { id: 5, descricao: 'Nescau 400gr', valor: 8.0, marca: 'Nestlé' },
+  ],
+};
+
+function generateUniqueId() {
+  return Math.floor(Math.random() * 100).toFixed();
+}
+
+app.post('/produtos', (req, res) => {
+  const requestBody = req.body;
+  const newId = Number(generateUniqueId());
+
+  const newProduct = { id: newId, ...requestBody };
+
+  lista_produtos.produtos.push(newProduct);
+
+  const createdResource = newProduct;
+  res.status(201).json(createdResource);
+});
+
+app.get('/produtos', (req, res) => {
+  res.send(lista_produtos.produtos);
+});
+
+app.get('/produtos/:idProduct', (req, res) => {
+  const { idProduct } = req.params;
+
+  const getProduct = lista_produtos.produtos.find(
+    ({ id }) => id === Number(idProduct)
+  );
+
+  res.send(getProduct);
+});
+
+app.put('/produtos/:idProduct', (req, res) => {
+  const { id, descricao, valor, marca } = req.body;
+  const { idProduct } = req.params;
+
+  const getProductIndex = lista_produtos.produtos.findIndex(
+    ({ id }) => id === Number(idProduct)
+  );
+
+  if (getProductIndex !== -1) {
+    const product = lista_produtos.produtos[getProductIndex];
+    lista_produtos.produtos[getProductIndex] = {
+      id: product.id,
+      descricao: !descricao ? product.descricao : descricao,
+      valor: !valor ? product.valor : valor,
+      marca: !marca ? product.marca : marca,
+    };
+    res.send(lista_produtos.produtos[getProductIndex]);
+  } else {
+    res.status(400).json('Product with the specified ID was not found.');
+  }
+});
+
+app.delete('/produtos/:idProduct', (req, res) => {
+  const { idProduct } = req.params;
+
+  const getProductIndex = lista_produtos.produtos.findIndex(
+    ({ id }) => id === Number(idProduct)
+  );
+
+  if (getProductIndex !== -1) {
+    lista_produtos.produtos = lista_produtos.produtos.filter(
+      (product) => product.id !== Number(idProduct)
+    );
+    console.log(lista_produtos.produtos);
+    res.send('Success');
+  } else {
+    res.status(400).json('Product with the specified ID was not found.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
